36854e128bbb36475ef46a88657adf8008084eec
LizarKlaun <bearodit@gmail.com>
2024-11-11 15:46:56 +0200

    Fixed a bug when it summed and swapped incorrectly

--- 2048/2048.cpp
+++ 2048/2048.cpp
@@ -35,32 +35,35 @@ int** playesMoveOutput(int** field, int countingMoves, char move){
             for (int j = 0; j < 4; j++) {
                 if (field[i][j] != 0) {
                     if (field[0][j] == field[i][j] && i != 0) {
-                        cout << "plus" << endl;
+                        cout << "plus : 0 : 0" << endl;
                         field[0][j] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
-                    else if (field[1][j] == field[i][j] && i != 1) {
-                        cout << "plus" << endl;
+                    else if (field[1][j] == field[i][j] && i != 0 && i != 1) {
+                        cout << "plus : 0 : 1" << endl;
                         field[1][j] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
-                    else if (field[2][j] == field[i][j] && i != 2) {
-                        cout << "plus" << endl;
+                    else if (field[2][j] == field[i][j] && i != 0 && i != 1 && i != 2) {
+                        cout << "plus : 0 : 2" << endl;
                         field[2][j] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
                     else if (field[0][j] == 0) {
-                        cout << "reverse" << endl;
+                        cout << "reverse : 0 : 0 . j: " << i << ". j: " << j << endl;
                         field[0][j] = field[i][j];
                         field[i][j] = 0;
                     }
                     else if (field[1][j] == 0) {
-                        cout << "reverse" << endl;
+                        cout << "reverse : 0 : 1 . j: " << i << ". j: " << j << endl;
                         field[1][j] = field[i][j];
                         field[i][j] = 0;
                     }
-                    else if (field[2][j] == 0) {
-                        cout << "reverse" << endl;
+                    else if (field[2][j] == 0 && i != 0) {
+                        cout << "reverse : 0 : 2 . j: " << i << ". j: " << j << endl;
                         field[2][j] = field[i][j];
                         field[i][j] = 0;
                     }
@@ -68,37 +71,41 @@ int** playesMoveOutput(int** field, int countingMoves, char move){
             }
         }
     }
+
     else if (move == 'a' || move == 'A') {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 if (field[i][j] != 0) {
                     if (field[i][0] == field[i][j] && j != 0) {
-                        cout << "plus" << endl;
+                        cout << "plus : 1 : 0" << endl;
                         field[i][0] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
-                    else if (field[i][1] == field[i][j] && j != 1) {
-                        cout << "plus" << endl;
+                    else if (field[i][1] == field[i][j] && j != 0 && j != 1) {
+                        cout << "plus : 1 : 1" << endl;
                         field[i][1] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
-                    else if (field[i][2] == field[i][j] && j != 2) {
-                        cout << "plus" << endl;
+                    else if (field[i][2] == field[i][j] && j != 0 && j != 1 && j != 2) {
+                        cout << "plus : 1 : 2" << endl;
                         field[i][2] += field[i][j];
                         field[i][j] = 0;
+                        break;
                     }
                     else if (field[i][0] == 0) {
-                        cout << "reverse" << endl;
+                        cout << "reverse : 1 : 0 . i: " << i << ". j: " << j << endl;
                         field[i][0] = field[i][j];
                         field[i][j] = 0;
                     }
                     else if (field[i][1] == 0) {
-                        cout << "reverse" << endl;
+                        cout << "reverse : 1 : 1 . j: " << i << ". j: " << j << endl;
                         field[i][1] = field[i][j];
                         field[i][j] = 0;
                     }
-                    else if (field[i][2] == 0) {
-                        cout << "reverse" << endl;
+                    else if (field[i][2] == 0 && j != 0) {
+                        cout << "reverse : 1 : 2 . j: " << i << ". j: " << j << endl;
                         field[i][2] = field[i][j];
                         field[i][j] = 0;
                     }
@@ -131,7 +138,7 @@ int main()
 
     char move = 0;
     int countingMoves = 1;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 20; i++)
     {
         playesMoveOutput(field, countingMoves, move);
     }
